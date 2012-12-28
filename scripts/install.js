20083dc64ef208bb5ad63ab9f19f87cf109fad8c
Justin <jrussell@ivantagehealth.com>
2012-12-28 16:15:26 -0500

    cleaning up and modularizing

--- scripts/install.js
+++ scripts/install.js
@@ -1,25 +1,2 @@
-var fs = require( "fs" )
-	, appPkg = "../../package.json"
-	, pkg = JSON.parse( fs.readFileSync( appPkg ).toString() )
-	, installScripts
-	, helperScript
-	;
+require( "./lib/hooks" )( "install" );
 
-// -----------------------------------------------------
-// Add a install hook for svn scripts if it isn't already there in the main
-// app's package.js
-// -----------------------------------------------------
-pkg.scripts = pkg.scripts || {};
-pkg.scripts.install = pkg.scripts.install || "";
-
-installScripts = pkg.scripts.install.split( "&&" ).map( function( scrp ) {
-	return scrp.trim();
-});
-
-helperScript = "node ./node_modules/svn-npm-crutch/lib/svn-npm-crutch.js";
-
-if( -1 === installScripts.indexOf( helperScript ) ) {
-	installScripts.push( helperScript );
-	pkg.scripts.install = installScripts.join( " && " );
-	fs.writeFileSync( appPkg, JSON.stringify( pkg, null, "\t" ) );
-}
---
+++ scripts/lib/hooks.js
@@ -0,0 +1,36 @@
+module.exports = function( hook ) {
+	var fs = require( "fs" )
+		, appPkg = "../../package.json"
+		, pkg = JSON.parse( fs.readFileSync( appPkg ).toString() )
+		, installScripts = []
+		, helperScript
+		;
+
+	// -----------------------------------------------------
+	// Add a install hook for svn scripts if it isn't already there in the main
+	// app's package.js
+	// -----------------------------------------------------
+	pkg.scripts = pkg.scripts || {};
+	pkg.scripts.install = pkg.scripts.install || "";
+
+	pkg.scripts.install.split( "&&" ).forEach( function( scrp ) {
+		// -----------------------------------------------------
+		// Clear out any older install scripts for svn-npm-crutch
+		// -----------------------------------------------------
+		if( -1 === scrp.indexOf( "node_modules/svn-npm-crutch" ) ) {
+			scrp = scrp.trim();
+			if( scrp.length > 0 ) {
+				installScripts.push( scrp );
+			}
+		}
+	});
+
+	if( "install" === hook ) {
+		helperScript = "node ./node_modules/svn-npm-crutch/lib/svn-npm-crutch.js";
+		installScripts.push( helperScript );
+	}
+
+	pkg.scripts.install = installScripts.join( " && " );
+	fs.writeFileSync( appPkg, JSON.stringify( pkg, null, "\t" ) );
+};
+
