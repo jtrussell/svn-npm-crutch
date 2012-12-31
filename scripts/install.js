522967c39c522144c1b185e7a55adbeacbbb41ec
Justin <jrussell@ivantagehealth.com>
2012-12-31 15:56:14 -0500

    Run install script on initial install

--- scripts/install.js
+++ scripts/install.js
@@ -1,2 +1,10 @@
+// -----------------------------------------------------
+// Update the project's package.json to run our install script when their
+// install runs
+// -----------------------------------------------------
 require( "./lib/hooks" )( "install" );
 
+require( "child_process" )
+	.exec( "node ../lib/svn-npm-crutch.js", {
+		cwd: __dirname
+	});
